98eadece2600458e6d03bb92c094a8b4284e40b6
Spencer <swakefield4786@westfield.ma.edu>
2023-02-20 15:33:31 -0500

    update 2

--- src/main/java/edu/wsu/App.java
+++ src/main/java/edu/wsu/App.java
@@ -1,12 +1,18 @@
 package edu.wsu;
 
+import edu.wsu.model.Players;
 import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -21,9 +27,10 @@ public class App extends Application {
 
     private static Scene scene;
 
-    public static List<String> names = Arrays.asList(
-            "Daph", "Jack", "Casey", "Lucas", "Ivan", "Gavin"
-    );
+    int currentPlayer = 0;
+
+
+    ObservableList<Players> players;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -33,12 +40,22 @@ public class App extends Application {
         gridPane.setVgap(10);
         gridPane.setPadding(new Insets(10));
 
+        List<Players> playerList = Arrays.asList(
+                new Players("innocent", "daph"),
+                new Players("murderer", "jason"),
+                new Players("cop", "casey"),
+                new Players("cop", "lucas"),
+                new Players("cop", "ivan"),
+                new Players("cop", "gavin")
+        );
+        players = FXCollections.observableList(playerList);
+
         //Loops to fill out the table
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 3; j++) {
                 int index = i * 3 + j;
-                if (index < names.size()) {
-                    Button button = new Button(names.get(index));
+                if (index < players.size()) {
+                    Button button = new Button(players.get(index).playername);
                     gridPane.add(button, j, i);
 
                     //disables the button once picked to simulate it being voted out or killed
@@ -48,6 +65,14 @@ public class App extends Application {
                 }
             }
         }
+        ObservableList<String> currentPlayerOptions = FXCollections.observableList(players.get(currentPlayer).actionOption);
+        ScrollPane optionsDisplay = new ScrollPane();
+        ListView<String> options = new ListView<>();
+        options.setItems(currentPlayerOptions);
+        optionsDisplay.setContent(options);
+
+        gridPane.add(optionsDisplay, 1,1);
+
         //code for the skip button
         GridPane bottomPane = new GridPane();
         bottomPane.setAlignment(Pos.CENTER);

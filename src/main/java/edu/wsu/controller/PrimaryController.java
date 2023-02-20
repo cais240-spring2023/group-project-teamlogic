7f44606665390d7032110e4f5e7c6534cf9bb28b
Dan-Shlykov <113049975+Dan-Shlykov@users.noreply.github.com>
2023-02-19 20:36:31 -0500

    Update PrimaryController.java
    A placeholder for the javafx code for now

--- src/main/java/edu/wsu/controller/PrimaryController.java
+++ src/main/java/edu/wsu/controller/PrimaryController.java
@@ -6,6 +6,70 @@ import javafx.fxml.FXML;
 
 public class PrimaryController {
 
+
+//This is where Im going to put the java fx code for now
+package org.example;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import java.util.Arrays;
+import java.util.List;
+
+public class ButtonTable extends Application {
+
+
+    //Needs a list of names to put into the buttons
+    public static List<String> names = Arrays.asList(
+            "Daph", "Jack", "Casey", "Lucas", "Ivan", "Gavin"
+            );
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Create a 2x3 grid pane
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(10));
+
+        //Loops to fill out the table
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                int index = i * 3 + j;
+                if (index < names.size()) {
+                    Button button = new Button(names.get(index));
+                    gridPane.add(button, j, i);
+
+                    //disables the button once picked to simulate it being voted out or killed
+                    button.setOnAction(event -> {
+                        button.setDisable(true);
+                    });
+                }
+            }
+        }
+        //code for the skip button
+        GridPane bottomPane = new GridPane();
+        bottomPane.setAlignment(Pos.CENTER);
+
+        Button bottomButton = new Button("Skip Turn");
+        bottomButton.setOnAction(event -> {
+            System.out.println("Turn has been skipped");
+        });
+
+        //Window maker
+        Scene scene = new Scene(gridPane, 400, 300);
+        scene.getStylesheets().add("/styles/Styles.css");
+        stage.setTitle("Player List");
+        stage.setScene(scene);
+        stage.show();
+
+            }
+        }
+
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");

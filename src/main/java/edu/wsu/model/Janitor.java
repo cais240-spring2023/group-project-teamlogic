5f8eb8fec3a39520d349dedddc17635c18d1b80c
EShannon-create <123516564+EShannon-create@users.noreply.github.com>
2023-03-30 08:03:35 -0400

    Update Janitor.java

--- src/main/java/edu/wsu/model/Janitor.java
+++ src/main/java/edu/wsu/model/Janitor.java
@@ -4,5 +4,24 @@ public class Janitor extends Murderer{
     public Janitor(String name) {
         super(name);
     }
+    @Override
+    public void tellRole(){
+        hear("You are a janitor.");
+    }
 
+    @Override
+    public String getNightActionName(){
+        return "clean";
+    }
+    public boolean hasAction(){
+        return true;
+    }
+    @Override
+    public String roleString(){
+        return "a janitor.";
+    }
+    @Override
+    public void nightHandler(Player acted){
+        acted.clean();
+    }
 }

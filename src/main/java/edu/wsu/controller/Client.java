1ad87718e71acfdcb8fb351b00210db5c3bfc78b
EShannon-create <123516564+EShannon-create@users.noreply.github.com>
2023-04-12 12:45:27 -0400

    Small fixes

--- src/main/java/edu/wsu/controller/Client.java
+++ src/main/java/edu/wsu/controller/Client.java
@@ -9,29 +9,6 @@ public class Client {
     private static Socket socket;
     private static PrintWriter out;
     private static BufferedReader in;
-    public static void main(String[] args) throws IOException {
-        String hostName = "localhost"; // The IP address of the server
-        int portNumber = 4544; // The port number the server is listening on
-
-        try (
-                Socket socket = new Socket(hostName, portNumber);
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        ) {
-            // Send a message to the server
-            out.println("Hello, server!");
-
-            // Receive the server's response
-            String response = in.readLine();
-            System.out.println("Server: " + response);
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + hostName);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " + hostName);
-            System.exit(1);
-        }
-    }
     public static void launchClient() {
         try {
             socket = new Socket(hostName, portNumber);
